8f318dbe5493385cda4173762e40470db37015eb
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-10-12 21:54:24 +0900

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/explicit-function-return-type': 2,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-self-closing': 0,
+    'vue/attributes-order': 0
+  }
+}
