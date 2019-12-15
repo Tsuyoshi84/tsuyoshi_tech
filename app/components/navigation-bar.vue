16fd835b44496ca0a31369169184b42c25a23e39
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-12-15 11:57:31 +0900

    Modify link style

--- app/components/navigation-bar.vue
+++ app/components/navigation-bar.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   header.header
-    nuxt-link(to='/') About
-    nuxt-link(to='/experience') Experience
-    nuxt-link(to='/skills') Skills
-    nuxt-link(to='/projects') Projects
-    nuxt-link(to='/contact') Contact
+    nuxt-link.link(to='/') About
+    nuxt-link.link(to='/experience') Experience
+    nuxt-link.link(to='/skills') Skills
+    nuxt-link.link(to='/projects') Projects
+    nuxt-link.link(to='/contact') Contact
 </template>
 
 
@@ -19,4 +19,11 @@ export default class extends Vue {}
 <style lang="sass" scoped>
 .header
   display: flex
+  justify-content: center
+  padding-top: 1.5rem
+  padding-bottom: 1.5rem
+
+  .link
+    margin: 0 1rem
+    font-size: 2.0rem
 </style>
