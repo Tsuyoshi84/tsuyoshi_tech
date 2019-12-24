791a15be88036a87cc75fd21f98a1abde162b57b
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-12-25 00:19:15 +0900

    Fix tests

--- app/test/components/social-media-icon.spec.ts
+++ app/test/components/social-media-icon.spec.ts
@@ -1,13 +1,38 @@
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import SocialMediaIcon from '@/components/social-media-icon.vue'
+
+const getComponent = () => {
+  return mount(SocialMediaIcon, {
+    propsData: { href: 'https://example.com', color: 'red' },
+    slots: {
+      default: '<div>slot-content</div>'
+    },
+    stubs: {}
+  })
+}
 
 describe('SocialMediaIcon', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(SocialMediaIcon, {
-      stubs: {
-        NuxtLink: RouterLinkStub
-      }
-    })
+    const wrapper = getComponent()
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  describe('when not hover', () => {
+    test('should use default color', done => {
+      const wrapper = getComponent()
+      expect(wrapper.html()).toMatchSnapshot()
+      done()
+    })
+  })
+
+  describe('when hover', () => {
+    test('should use given color', done => {
+      const wrapper = getComponent()
+      wrapper.trigger('mouseover')
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.html()).toMatchSnapshot()
+        done()
+      })
+    })
   })
 })
--- app/test/layouts/default.spec.ts
+++ app/test/layouts/default.spec.ts
@@ -3,7 +3,9 @@ import Default from '@/layouts/default.vue'
 
 describe('Default', () => {
   test('is a Vue instance', () => {
-    const wrapper = shallowMount(Default)
+    const wrapper = shallowMount(Default, {
+      stubs: ['nuxt']
+    })
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
