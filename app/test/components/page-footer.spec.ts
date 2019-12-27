be796b7efff2c70a27b06a7cbeb56530c4a19575
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2019-12-28 02:00:40 +0900

    Add PageFooter test

---
+++ app/test/components/page-footer.spec.ts
@@ -0,0 +1,22 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import PageFooter from '@/components/page-footer.vue'
+
+const makeWrapper = () => {
+  return mount(PageFooter, {
+    stubs: {
+      NuxtLink: RouterLinkStub
+    }
+  })
+}
+
+describe('PageFooter', () => {
+  test('is a Vue instance', () => {
+    const wrapper = makeWrapper()
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('generate DOM', () => {
+    const wrapper = makeWrapper()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
