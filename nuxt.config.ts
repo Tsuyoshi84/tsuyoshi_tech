7c8dcf329dcb0e3d4b0e50f616a20eec97fe05ea
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-06-10 23:20:15 +0900

    Move google font to global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,14 +21,6 @@ const config: Configuration = {
         content: 'Tsuyoshi Yamaguchi',
       },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap',
-      },
-    ],
   },
 
   /*
