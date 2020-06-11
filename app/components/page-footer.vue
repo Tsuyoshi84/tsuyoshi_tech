6ff2407355bf9259f5a1344f6f1b51fc9ec77df9
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-06-11 22:45:16 +0900

    Remove footer

--- app/components/page-footer.vue
+++ app/components/page-footer.vue
@@ -1,7 +1,5 @@
 <template lang="pug">
 footer
-  a.code-link(href='https://github.com/Tsuyoshi84/tsuyoshi_tech' alt='Source code')
-    include ../assets/svgs/github.svg
     
 </template>
 
@@ -12,22 +10,4 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class extends Vue {}
 </script>
 
-<style lang="sass" scoped>
-$font-color: #aaa
-
-footer
-  height: 6rem
-  width: 100%
-  padding: 2rem
-  text-align: center
-  color: $font-color
-
-  .code-link
-    width: 2rem
-    height: 2rem
-    display: inline-block
-    color: $font-color
-
-    svg
-      fill: currentColor
-</style>
+<style lang="sass" scoped></style>
